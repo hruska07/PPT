a0098f9621cbe07167aa6602c38a477537f420f0
Tomáš Cink <cink01@student.vspj.cz>
2018-11-22 15:27:42 +0100

    cvicení 3 sady 2 ukol 9

---
+++ sada_ukolu_2/Cviceni_09/cviceni9/cviceni9/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace cviceni9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_START = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_START
+            // 
+            this.bt_START.ForeColor = System.Drawing.Color.Red;
+            this.bt_START.Location = new System.Drawing.Point(12, 12);
+            this.bt_START.Name = "bt_START";
+            this.bt_START.Size = new System.Drawing.Size(540, 214);
+            this.bt_START.TabIndex = 0;
+            this.bt_START.Text = "START";
+            this.bt_START.UseVisualStyleBackColor = true;
+            this.bt_START.Click += new System.EventHandler(this.bt_START_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 237);
+            this.Controls.Add(this.bt_START);
+            this.Location = new System.Drawing.Point(50, 50);
+            this.Name = "Form1";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_START;
+    }
+}
+
---
+++ sada_ukolu_2/Cviceni_09/cviceni9/cviceni9/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace cviceni9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static bool FileEquals(string path1, string path2)
+        {
+            byte[] file1 = File.ReadAllBytes(path1);
+            byte[] file2 = File.ReadAllBytes(path2);
+            if (file1.Length == file2.Length)
+            {
+                for (int i = 0; i < file1.Length; i++)
+                    if (file1[i] != file2[i])
+                        return false;
+                return true;
+            }
+            return false;
+        }
+
+        private void bt_START_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (FileEquals(@"H:\vspj\5 semestr\PTT\PPT\sada_ukolu_2\Cviceni_09\zadani\vzor_vysledku\_overview__svm_00_orig.csv",
+                    @"H:\vspj\5 semestr\PTT\PPT\sada_ukolu_2\Cviceni_09\data\_results_data\svm_00\_overview__svm_00_orig.csv"))
+                    MessageBox.Show("Soubory jsou stejne", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Soubory nejsou stejne", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch
+            {
+                MessageBox.Show("Soubory nejsou stejne nebo není vygenerován", "Info", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
