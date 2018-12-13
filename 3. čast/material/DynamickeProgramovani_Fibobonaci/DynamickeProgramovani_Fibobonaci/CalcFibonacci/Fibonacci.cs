1bc9ab48ef8d0cd28901a7d3a71bb80ae7110c51
Tomáš Cink <cink01@student.vspj.cz>
2018-12-13 14:46:05 +0100

    3. část

---
+++ 3. čast/material/DynamickeProgramovani_Fibobonaci/DynamickeProgramovani_Fibobonaci/CalcFibonacci/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcFibonacci
+{
+    class Fibonacci
+    {
+        public int CalcRek(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcRekTable(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CalcNerek(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
