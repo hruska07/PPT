fa06af486536322055d878f3d9f4da521f7abd61
Jáchym Hruška <hruska07@student.vspj.cz>
2019-01-02 19:28:33 +0100

    3. sada ukolu - dokonceni

--- 3. čast/material/DynamickeProgramovani_Fibobonaci/DynamickeProgramovani_Fibobonaci/CalcFibonacci/Fibonacci.cs
+++ 3. čast/material/DynamickeProgramovani_Fibobonaci/DynamickeProgramovani_Fibobonaci/CalcFibonacci/Fibonacci.cs
@@ -5,14 +5,23 @@ namespace CalcFibonacci
 {
     class Fibonacci
     {
-     //   public int[] DataFibona { get; private set; }
-        public int CalcRek(int n)
+        //public int[] DataFibona { get; private set; }
+
+        public int CalcRek(int n, ref int count, ref int count_rekurze)
         {
-            /*if ( n==1||n == 2)
+            count += 3;
+            if (n == 1 || n == 2)
+            {
                 return 1;
+            }
             else
-                return CalcRek(n - 2) + CalcRek(n - 1);*/
-            return n == 1 || n == 2 ? 1 : CalcRek(n - 2) + CalcRek(n - 1);
+            {
+                count += 3;
+                count_rekurze += 2;
+                return CalcRek(n - 2, ref count, ref count_rekurze) + CalcRek(n - 1, ref count, ref count_rekurze);
+            }
+
+            //return n == 1 || n == 2 ? 1 : CalcRek(n - 2) + CalcRek(n - 1);
         }
         public Fibonacci(){}
         /*
@@ -23,35 +32,47 @@ namespace CalcFibonacci
                 DataFibona[i] = -1;
         }*/
 
-        public int CalcRekTable(int n)
+        public int CalcRekTable(int n, ref int count, ref int count_rekurze)
         {
+            count += 1;
             if (n <= 1)
                 return n;
+            count += 2;
             int[] fib = new int[n + 1];//tvorba pomocneho pole
-            return CalcRekTable(fib, n);
+            return CalcRekTable(fib, n, ref count, ref count_rekurze);
         }
 
-        private int CalcRekTable(int[] fib, int n)
+        private int CalcRekTable(int[] fib, int n, ref int count, ref int count_rekurze)
         {
             // jestli je spocitany nebo n mensi nebo rovno 1tak to vraci hodnotu 
+            count += 1;
             if (n <= 1)
                 return n;
+            count += 2;
             if (fib[n] > 0)
+            {
+                count += 1;
                 return fib[n];
+            }
+            
+            count += 6;
+            count_rekurze += 2;
             //pocitani a vracení nevyplnenych hodnot
-            return fib[n] = CalcRekTable(fib, n - 1) + CalcRekTable(fib, n - 2);
+            return fib[n] = CalcRekTable(fib, n - 1, ref count, ref count_rekurze) + CalcRekTable(fib, n - 2, ref count, ref count_rekurze);
         }
 
-        public int CalcNerek(int n)
+        public int CalcNerek(int n, ref int count)
         {
             int a = 1;
             int b = 1;
             int c;
+            count += 2 + 2;
             for (int i = 3; i <= n; i++)
             {
                 c = a;
                 a = b + a;
                 b = c;
+                count += 4 + 3;
             }
             return a;
         }
--- 3. čast/material/DynamickeProgramovani_Fibobonaci/DynamickeProgramovani_Fibobonaci/CalcFibonacci/Program.cs
+++ 3. čast/material/DynamickeProgramovani_Fibobonaci/DynamickeProgramovani_Fibobonaci/CalcFibonacci/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CalcFibonacci;
+using System.Diagnostics;
 
 namespace CalcFibonacci
 {
@@ -18,9 +19,36 @@ namespace CalcFibonacci
                 n = Convert.ToInt32(Console.ReadLine());
             } while (n <= 0);
             Fibonacci fib = new Fibonacci();
-            Console.Write(fib.CalcRek(Convert.ToInt32(n)).ToString()+"\n");
-            Console.Write(fib.CalcNerek(Convert.ToInt32(n)).ToString() + "\n");
-            Console.Write(fib.CalcRekTable(Convert.ToInt32(n)).ToString() + "\n");
+            Stopwatch watch = new Stopwatch();
+            int vysl1, vysl2, vysl3;
+            int sloz1 = 0;
+            int sloz2 = 0;
+            int sloz3 = 0;
+            int count_rekurze1 = 0;
+            int count_rekurze3 = 0;
+
+            TimeSpan time1, time2, time3;
+            watch.Start();
+            vysl1 = fib.CalcRek(Convert.ToInt32(n), ref sloz1, ref count_rekurze1);
+            watch.Stop();
+            time1 = watch.Elapsed;
+
+            watch.Restart();
+            vysl2 = fib.CalcNerek(Convert.ToInt32(n), ref sloz2);
+            watch.Stop();
+            time2 = watch.Elapsed;
+
+            watch.Restart();
+            vysl3 = fib.CalcRekTable(Convert.ToInt32(n), ref sloz3, ref count_rekurze3);
+            watch.Stop();
+            time3 = watch.Elapsed;
+
+            Console.Write("FIBONACCI - REKURZIVNE (bez tabulky):\n");
+            Console.Write(String.Format("Vysledek: {0}; Slozitost: {1}; Cas: {2}; Pocet rekurzi: {3}\n\n", vysl1, sloz1, time1, count_rekurze1));
+            Console.Write("FIBONACCI - NEREKURZIVNE:\n");
+            Console.Write(String.Format("Vysledek: {0}; Slozitost: {1}; Cas: {2}\n\n", vysl2, sloz2, time2));
+            Console.Write("FIBONACCI - REKURZIVNE (s tabulkou):\n");
+            Console.Write(String.Format("Vysledek: {0}; Slozitost: {1}; Cas: {2}; Pocet rekurzi: {3}\n\n", vysl3, sloz3, time3, count_rekurze3));
             Console.Write("PRO UKONCENI STISKNETE ENTER");
             try
             {
